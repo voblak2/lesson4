e7195134c716ea0f57504e5c9a2d1d5a6f67a78c
Vince Oblak <voblak2@gmail.com>
2020-10-14 20:40:41 -0500

    More newer commit

--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ namespace lesson4
             int num1 = 1;
             int num2 = 2;
 
-            // store the returned value of the "add" method
+            // store the returned value of the "add" method.
             int sum = Add(num1, num2);
             Console.WriteLine(sum);
         }
 
-        // add method returns an integer
+        // add method returns an integer.
         public static int Add(int number1, int number2)
         {
             return (number1 + number2);
